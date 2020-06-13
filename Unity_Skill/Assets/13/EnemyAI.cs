5f826700929e81ea0eac94452159fce929c8d7c2
tkd14gus <qkrtkdgus78@gmail.com>
2020-06-14 02:50:51 +0900

    AI순찰과 거점 방어
    AI가 순찰을 돌다가 일정 지역에 도달하면 적이 플레이어를 향해 다가오도록 구현하는 것을 따라하며 익혔다.

---
+++ Unity_Skill/Assets/13/EnemyAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    NavMeshAgent m_enemy = null;
+
+    [SerializeField] Transform[] m_tfWayPoint = null;
+    int m_count = 0;
+
+    Transform m_target = null;
+
+    public void SetTarget(Transform p_target)
+    {
+        CancelInvoke();
+        m_target = p_target;
+    }
+
+    public void RemoveTarget()
+    {
+        m_target = null;
+        InvokeRepeating("MoveToNextWayPoint", 0f, 2f);
+    }
+
+    void MoveToNextWayPoint()
+    {
+        if(m_target == null)
+        {
+            if (m_enemy.velocity == Vector3.zero)
+            {
+                m_enemy.SetDestination(m_tfWayPoint[m_count++].position);
+
+                if (m_count >= m_tfWayPoint.Length)
+                    m_count = 0;
+            }
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_enemy = GetComponent<NavMeshAgent>();
+        InvokeRepeating("MoveToNextWayPoint", 0f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_target != null)
+        {
+            m_enemy.SetDestination(m_target.position);
+        }
+    }
+}
