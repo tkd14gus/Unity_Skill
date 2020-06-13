ec7653cd762fd46a5d2182c87edbf0d8c4ecaded
tkd14gus <qkrtkdgus78@gmail.com>
2020-06-13 18:42:58 +0900

    오브젝트 풀링
    오브젝트를 미리 생성 후 활성화, 비활성화를 번갈아가며 사용하는 오브젝트 풀링을 익혔다.

---
+++ Unity_Skill/Unity_Skill/Assets/CreateCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateCorutine());
+    }
+
+    IEnumerator CreateCorutine()
+    {
+        while (true)
+        {
+            yield return null;
+            GameObject t_object = ObjectPooingManager.instance.GetQueue();
+            t_object.transform.position = Vector3.zero;
+        }
+    }
+}
